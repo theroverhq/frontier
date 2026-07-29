2eb40109d676cbab8625107617bbc5ad167c8af0
Dhruv Chaudhari <dhruvchaudhari@gmail.com>
2026-07-29 15:30:32 +0530

    feat: replace hero value cards with interactive SIEM routing blind-spot widget

--- serve.sh
+++ serve.sh
@@ -4,4 +4,4 @@ docker run --rm \
   --network host \
   --volume="$PWD:/srv/jekyll" \
   jekyll/jekyll \
-  sh -c "bundle install && bundle exec jekyll serve --livereload"
+  sh -c "bundle install && bundle exec jekyll serve --livereload --livereload-port 35730"
