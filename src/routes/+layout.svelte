685a552edd94b8662246f23255d6cb22be8f0f22
Dhruv Chaudhari <dhruvchaudhari@gmail.com>
2026-09-02 14:33:22 +0530

    feat: migrate analytics and clarity script injection to onMount in layout to improve hydration reliability

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,43 @@
   import "../app.css";
   import Navbar from "$lib/components/Navbar.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import { onMount } from "svelte";
+  import { PUBLIC_GA_MEASUREMENT_ID, PUBLIC_CLARITY_PROJECT_ID } from "$env/static/public";
+  
   let { children } = $props();
+
+  onMount(() => {
+    // Inject Google Analytics after hydration
+    if (PUBLIC_GA_MEASUREMENT_ID) {
+      const gaScript = document.createElement("script");
+      gaScript.async = true;
+      gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GA_MEASUREMENT_ID}`;
+      document.head.appendChild(gaScript);
+
+      const gaConfig = document.createElement("script");
+      gaConfig.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${PUBLIC_GA_MEASUREMENT_ID}');
+      `;
+      document.head.appendChild(gaConfig);
+    }
+
+    // Inject Microsoft Clarity after hydration
+    if (PUBLIC_CLARITY_PROJECT_ID) {
+      const clarityConfig = document.createElement("script");
+      clarityConfig.type = "text/javascript";
+      clarityConfig.innerHTML = `
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "${PUBLIC_CLARITY_PROJECT_ID}");
+      `;
+      document.head.appendChild(clarityConfig);
+    }
+  });
 </script>
 
 <svelte:head>
